0fc727ec02d511e2a48120bd2cd9ca1439d95a22
hpasham12 <hpasham12@gmail.com>
2026-04-15 15:05:28 -0500

    add github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/wedding-portal/',
   plugins: [react()],
   resolve: {
     alias: {
